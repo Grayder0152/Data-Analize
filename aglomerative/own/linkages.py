92225bae263ea478f42699b4368b1b520ed847c1
serhii_derevianko <serhii.derevianko_epam@kantar.com>
2023-12-21 02:38:06 +0200

    Added base realization of ward method;

--- aglomerative/own/linkages.py
+++ aglomerative/own/linkages.py
@@ -3,10 +3,13 @@ from abc import ABC, abstractmethod
 from enum import Enum
 
 import numpy as np
+import pyspark.sql.functions as f
+
 from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.linalg import DenseVector
+from pyspark.ml.functions import array_to_vector
+
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.types import ArrayType, DoubleType
 
 
 class LinkageMethodName(Enum):
@@ -24,72 +27,78 @@ class WardLinkage(LinkageMethod):
     name = LinkageMethodName.ward
 
     def linkage(self, data):
-        clusters = [[i.person_id] for i in data.select('person_id').collect()]
-
-        while len(clusters) > 1:
-            # Calculate the variance for each cluster
-            variances = [np.var(data[cluster], axis=0) for cluster in clusters]
-
-            # Calculate the pairwise squared Euclidean distances between cluster centroids
-            distances = np.zeros((len(clusters), len(clusters)))
-            for i in range(len(clusters)):
-                for j in range(i + 1, len(clusters)):
-                    centroid_diff = np.mean(data[clusters[i]], axis=0) - np.mean(data[clusters[j]], axis=0)
-                    distances[i, j] = np.sum(centroid_diff ** 2)
-                    distances[j, i] = distances[i, j]
-
-            # Find the indices of the two clusters with the smallest distance
-            min_i, min_j = np.unravel_index(np.argmin(distances), distances.shape)
-
-            # Merge the two clusters
-            clusters[min_i] += clusters[min_j]
-            del clusters[min_j]
-
-        return clusters[0]
-
-    @staticmethod
-    def ward_linkage(data):
-        # variance_udf = spark.udf.register("variance_udf", lambda x: float(np.var(x.toArray())))
-        centroids_udf = spark.udf.register("centroids_udf", lambda x, y: float(np.sum((x.toArray() - y.toArray()) ** 2)))
-        df = data.withColumn("cluster", col("features"))
-
-        while df.count() > 1:
-            # Calculate the variance for each cluster
-            # df = df.withColumn("variance", variance_udf(col("cluster")))
-
-            # Calculate the pairwise squared Euclidean distances between cluster centroids
-
-            df = df.withColumnRenamed("cluster", "cluster_1").withColumnRenamed("person_id", "person_id_1").crossJoin(
-                df.withColumnRenamed("cluster", "cluster_2").withColumnRenamed("person_id", "person_id_2"))
-            df = df.withColumn("centroid_distance", centroids_udf(col("cluster_1"), col("cluster_2")))
-            df = df.filter("cluster_1 != cluster_2")
-
-            # Find the indices of the two clusters with the smallest distance
-            min_distance_row = df.orderBy("centroid_distance").first()
-            min_cluster_1 = min_distance_row.person_id_1
-            min_cluster_2 = min_distance_row.person_id_2
-
-            # Merge the two clusters
-            df = df.filter("person_id_1 != '{}' AND person_id_2 != '{}'".format(min_cluster_1, min_cluster_2))
-            merged_cluster = df.union(min_cluster_1.union(min_cluster_2)).distinct()
-            df = df.union(merged_cluster)
-
-        # Show the final cluster
-        result = df.select("cluster").distinct().collect()
-        return result
+        pass
 
 
 if __name__ == '__main__':
+    import time
+
     spark = (
         SparkSession.builder.master("local[*]")
         .appName("K-mean")
+        .config("spark.sql.execution.arrow.pyspark.enabled", True)
         .getOrCreate()
     )
 
     assembler = VectorAssembler(inputCols=['t32', 't33'], outputCol="features")
     data = (
         spark.read.csv('../../data/data.csv', header=True, inferSchema=True)
-        .withColumnRenamed('Напряжение углекислого газа (PCO2)', 'person_id')
+        .withColumnRenamed('Напряжение углекислого газа (PCO2)', 'cluster_id')
     )
-    data = assembler.transform(data).select('person_id', 'features')
-    WardLinkage.ward_linkage(data)
+    data = assembler.transform(data).select('cluster_id', 'features')
+    data = data.limit(20).groupBy('cluster_id').agg(f.collect_list('features').alias('points'))
+
+
+    @f.udf(returnType=ArrayType(DoubleType()))
+    def calc_centroids(vectors):
+        np_vectors = np.array([v.toArray() for v in vectors])
+        centroid = np.mean(np_vectors, axis=0)
+        return centroid.tolist()
+
+
+    @f.udf(returnType=DoubleType())
+    def sum_of_distances_to_centroid(vectors, centroid):
+        centroid = np.array(centroid)
+        points = np.array([v.toArray() for v in vectors])
+        distances = np.sqrt(((points - centroid) ** 2).sum(axis=1))
+        return float(distances.sum())
+
+
+    i = data.count()
+    while i != 3:
+        print(f"{i} iteration")
+        start = time.time()
+        cached_data = data.cache()
+        cached_data = cached_data.withColumn('centroid', calc_centroids('points'))
+        cached_data = cached_data.withColumn('dist', sum_of_distances_to_centroid('points', 'centroid'))
+
+        df1 = cached_data.select(f.col('cluster_id').alias('cluster_id_1'), f.col('points').alias('points_1'),
+                                 f.col('dist'))
+        df2 = cached_data.select(f.col('cluster_id').alias('cluster_id_2'), f.col('points').alias('points_2'))
+
+        df = (
+            df1
+            .join(df2, f.col('cluster_id_1') != f.col('cluster_id_2'), 'cross')
+            .withColumn("sorted_ids", f.array_sort(f.array(f.col("cluster_id_1"), f.col("cluster_id_2"))))
+            .dropDuplicates(["sorted_ids"])
+            .withColumn('points', f.concat('points_1', 'points_2'))
+            .withColumn('centroid', calc_centroids('points'))
+            .withColumn('new_dist', sum_of_distances_to_centroid('points', 'centroid'))
+            .withColumn('diff', f.col('new_dist') - f.col('dist'))
+            .orderBy('diff')
+        )
+        new_centroid = df.select('cluster_id_1', 'cluster_id_2', 'points').first()
+        min_cluster_id, max_cluster_id = sorted([new_centroid.cluster_id_1, new_centroid.cluster_id_2])
+        new_points = f.array(*[array_to_vector(f.lit(vector.toArray())) for vector in new_centroid.points])
+        data = (
+            data
+            .where(f.col('cluster_id') != max_cluster_id)
+            .withColumn('points', f.when(
+                f.col('cluster_id') == min_cluster_id, new_points
+            ).otherwise(f.col('points')))
+        )
+
+        cached_data.unpersist()
+        i -= 1
+        print(time.time() - start)
+
